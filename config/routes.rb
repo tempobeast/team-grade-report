10da77271ea853de8d8bcdbf41de5cf10bc07ed0
Zach <shaw.zacht@gmail.com>
2023-05-31 08:49:21 -0700

    configure client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :courses
   resources :teachers
   resources :players
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  get '*path',
+    to: 'fallback#index',
+    constraints: ->(req) { !req.xhr? && req.format.html? }
 end
