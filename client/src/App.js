d0cc20bf1c86bdb3760ce2c18877b42472e74741
Zach <shaw.zacht@gmail.com>
2023-05-31 08:43:01 -0700

    init react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [players, setPlayers] = useState([]);
@@ -6,12 +7,18 @@ function App() {
   useEffect(() => {
     fetch("/players")
       .then((r) => r.json())
-      .then((data) => console.log(data));
+      .then((data) => setPlayers(data));
   }, []);
+
+  const displayPlayers = players.map((player) => <li>{`${player.last_name}, ${player.first_name}`}</li>)
 
   return (
     <div className="App">
       <h1>Team Grade Report</h1>
+      <Routes>
+        <Route path="/header" element={<h1>Header</h1>}/>
+      </Routes>
+      
     </div>
   );
 }
